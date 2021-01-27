d5ae22fad030bcbca9c796e9d873cac4649cf0ae
Alen Ribic <alen@sweepsouth.com>
2021-01-27 18:44:55 +0200

    Simplify db params (use db url instead).

--- pg_conn.ts
+++ pg_conn.ts
@@ -5,11 +5,7 @@ const Pool = require("pg").Pool
 
 export function makePool() {
   const pool = new Pool({
-    user: process.env.PG_DB_USER,
-    host: process.env.PG_DB_HOST,
-    database: process.env.PG_DB_NAME,
-    password: process.env.PG_DB_PASSWORD,
-    port: process.env.PG_DB_PORT,
+    connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
   })
   return pool
